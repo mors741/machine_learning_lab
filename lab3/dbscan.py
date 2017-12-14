296088c9fd45ed5868e9c93a000706e0309bc4d0
mors741 <Ub40sAnti7bhgLsd>
2017-12-14 18:36:24 +0300

    ari & heatmap

--- lab3/dbscan.py
+++ lab3/dbscan.py
@@ -2,19 +2,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import cm
 from sklearn.cluster import DBSCAN
-from sklearn.metrics import calinski_harabaz_score, silhouette_score
+from sklearn.metrics import calinski_harabaz_score, silhouette_score, adjusted_rand_score
 
+from lab3 import heat_map
 from lab3.visual import visualize
 
 def visualize_dbscan(eps, m_s, data, title):
     db = DBSCAN(eps=eps, min_samples=m_s).fit(data)
     visualize(data, db.labels_, title)
+    heat_map.run(data, db.labels_, title=title)
 
-def count_clusters_and_noise(data, labels):
+def count_clusters_and_noise(data, labels, true_labels):
     clusters = set()
     noise = 0
     clear_data = []
     clear_labels = []
+    clear_true_labels = []
     for i in xrange(len(labels)):
         if labels[i] == -1:
             noise += 1
@@ -22,19 +25,21 @@ def count_clusters_and_noise(data, labels):
             clusters.add(labels[i])
             clear_data.append(data[i])
             clear_labels.append(labels[i])
-    return len(clusters), noise, clear_data, clear_labels
+            clear_true_labels.append(true_labels[i])
+    return len(clusters), noise, clear_data, clear_labels, clear_true_labels
 
 
-def dbscan_params(eps, m_s, data):
+def dbscan_params(eps, m_s, data, true_labels):
     db = DBSCAN(eps=eps, min_samples=m_s).fit(data)
-    k, noise, clear_data, clear_labels = count_clusters_and_noise(data, db.labels_)
+    k, noise, clear_data, clear_labels, clear_true_labels = count_clusters_and_noise(data, db.labels_, true_labels)
     if k > 1:
         chi = calinski_harabaz_score(clear_data, clear_labels)
         si = silhouette_score(clear_data, clear_labels)
     else:
         chi = 1
         si = -1
-    return k, noise, chi, si
+    ari = adjusted_rand_score(clear_true_labels, clear_labels)
+    return k, noise, chi, si, ari
 
 
 def image_plot(X, Y, Z, title, z_label, max_chi=None, max_si=None, cmap=cm.jet, show_max=True):
@@ -61,13 +66,13 @@ def image_plot(X, Y, Z, title, z_label, max_chi=None, max_si=None, cmap=cm.jet, 
     plt.title(title)
 
 
-def print_max(name, max_val, E, M, K, N, CHI, SI):
+def print_max(name, max_val, E, M, K, N, CHI, SI, ARI):
     i, j = max_val[1:]
     print name + ": [eps=" + str(E[i][j]) + ", m_s=" + str(M[i][j]) + ", K=" + str(K[i][j]) \
-          + ", noise=" + str(N[i][j]) + ", CHI=" + str(CHI[i][j]) + ", SI=" + str(SI[i][j]) + "]"
+          + ", noise=" + str(N[i][j]) + ", CHI=" + str(CHI[i][j]) + ", SI=" + str(SI[i][j]) + ", ARI=" + str(ARI[i][j]) + "]"
 
 
-def run(data, eps_range, ms_range, cmap=cm.jet, show_max=True, noise_limit=1):
+def run(data, eps_range, ms_range, true_labels, cmap=cm.jet, show_max=True, noise_limit=1):
     sample_size = len(data)
     E, M = np.meshgrid(eps_range, ms_range)
     shape = np.shape(E)
@@ -75,16 +80,18 @@ def run(data, eps_range, ms_range, cmap=cm.jet, show_max=True, noise_limit=1):
     N = np.empty(shape)
     CHI = np.empty(shape)
     SI = np.empty(shape)
+    ARI = np.empty(shape)
     max_chi = (0, -1, -1)  # (chi, eps, m_s)
     max_si = (-1, -1, -1)  # (si, eps, m_s)
     for i in xrange(shape[0]):
         print str(M[i][0]) + "/" + str(M[-1][0])
         for j in xrange(shape[1]):
-            k, noise, chi, si = dbscan_params(E[i][j], M[i][j], data)
+            k, noise, chi, si, ari = dbscan_params(E[i][j], M[i][j], data, true_labels)
             K[i][j] = k
             N[i][j] = noise
             CHI[i][j] = chi
             SI[i][j] = si
+            ARI[i][j] = ari
             if sample_size * noise_limit >= noise:
                 if chi > max_chi[0]:
                     max_chi = (chi, i, j)
@@ -94,10 +101,11 @@ def run(data, eps_range, ms_range, cmap=cm.jet, show_max=True, noise_limit=1):
     image_plot(E, M, N, "Noise", "Noise", max_chi, max_si, cmap=cmap, show_max=show_max)
     image_plot(E, M, CHI, "Calinski-Harabasz index", "CHI", max_chi, max_si, cmap=cmap, show_max=show_max)
     image_plot(E, M, SI, "Silhouette Coefficient", "SI", max_chi, max_si, cmap=cmap, show_max=show_max)
+    image_plot(E, M, ARI, "Adjusted Rand index", "ARI", max_chi, max_si, cmap=cmap, show_max=show_max)
 
     print "--"
-    print_max("max_chi", max_chi, E, M, K, N, CHI, SI)
-    print_max("max_si", max_si, E, M, K, N, CHI, SI)
+    print_max("max_chi", max_chi, E, M, K, N, CHI, SI, ARI)
+    print_max("max_si", max_si, E, M, K, N, CHI, SI, ARI)
 
     best_chi_labels = DBSCAN(eps=E[max_chi[1]][max_chi[2]], min_samples=M[max_chi[1]][max_chi[2]]).fit(data).labels_
     best_si_labels = DBSCAN(eps=E[max_si[1]][max_si[2]], min_samples=M[max_si[1]][max_si[2]]).fit(data).labels_
@@ -107,5 +115,8 @@ def run(data, eps_range, ms_range, cmap=cm.jet, show_max=True, noise_limit=1):
         visualize(data, best_si_labels, "Max SI (" + "{:.3f}".format(SI[max_si[1]][max_si[2]]) + ")")
     else:
         print "Couldn't visualize clusterization result. Dimensions != 2"
+
+    heat_map.run(data, best_chi_labels, title="Heatmap best CHI")
+    heat_map.run(data, best_si_labels, title="Heatmap best SI")
 
     return best_chi_labels, best_si_labels
--- lab3/heat_map.py
+++ lab3/heat_map.py
@@ -6,28 +6,28 @@ from sklearn.metrics.pairwise import euclidean_distances
 from sklearn.metrics.pairwise import manhattan_distances
 
 
-def plot_heatmap(data, title='Heatmap'):
-    fig, ax = plt.subplots()
+def plot_heatmap(data, title):
+    plt.figure()
     plt.imshow(data, interpolation='nearest', cmap=cm.viridis)
     cb = plt.colorbar()
     cb.set_label("Closeness")
     plt.title(title)
     plt.ylabel('Samples')
     plt.xlabel('Samples')
-    ax.yaxis.set_minor_locator(AutoMinorLocator(2))
-    ax.xaxis.set_minor_locator(AutoMinorLocator(2))
-    plt.show()
 
 
-def run(data, distance='euclidean'):
-    cluster_index = data.shape[1] - 1
-    ind = np.argsort(data[:, cluster_index])
+def run(data, labels, distance='euclidean', title='Heatmap'):
+    data = np.array(data)
+    labels = np.array(labels)
+    noise_inds = np.where(labels == -1)
+    data = np.delete(data, noise_inds, 0)
+    labels = np.delete(labels, noise_inds)
+    ind = np.argsort(labels)
     data = data[ind]
-    data = np.delete(data, cluster_index, 1)
 
     if distance == 'euclidean':
         dist = euclidean_distances(data, data)
     if distance == 'manhattan':
         dist = manhattan_distances(data, data)
     closeness = np.subtract(1, np.divide(dist, np.max(dist)))
-    plot_heatmap(closeness)
+    plot_heatmap(closeness, title)
